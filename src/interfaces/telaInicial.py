88150f88b2259a828b47652f2f1384cebbb4f9b0
juan <bevilaquajuan@gmail.com>
2026-01-22 10:22:36 -0400

    inclusÃao dos elementos do frame direito

--- src/interfaces/telaInicial.py
+++ src/interfaces/telaInicial.py
@@ -20,13 +20,11 @@ class TelaInicial(ctk.CTkFrame):
         self.frame_esquerdo = ctk.CTkFrame(self)
         self.frame_esquerdo.grid(row=0, column=0, sticky="nsew")
 
-        self.frame_direito = ctk.CTkFrame(self)
+        self.frame_direito = ctk.CTkFrame(self, fg_color='#C0B8B8')
         self.frame_direito.grid(row=0, column=1, sticky="nsew")
 
-        #img_principal_path = 'src/static/img/capa tela inicial.png'
-
-        #self.inserir_imagem(img_principal_path)
-
+        # FRAME ESQUERDO
+        # Inserção da imagem principal no frame esquerdo
         img = Image.open('src/static/img/capa tela inicial.png')
         #self.img_principal = ImageTk.PhotoImage(img)
 
@@ -36,15 +34,13 @@ class TelaInicial(ctk.CTkFrame):
             size=(400, 600)  # tamanho desejado
         )
 
-        self.img_original_label = ctk.CTkLabel(self.frame_esquerdo, text='', image=self.img_principal)
-        self.img_original_label.grid(row=0, column=0, sticky='nsew')
+        self.img_principal_label = ctk.CTkLabel(self.frame_esquerdo, text='', image=self.img_principal)
+        self.img_principal_label.grid(row=0, column=0, sticky='nsew')
 
-        # ctk.CTkLabel(
-        #     self.frame_direito,
-        #     text="Conteúdo do sistema",
-        #     font=("Segoe UI", 22)
-        # ).pack(expand=True)
 
+        #FRAME DIREITO
+
+        # Configuração do background
         background = Image.open('src/static/img/background tela inicial.png')
         # self.img_principal = ImageTk.PhotoImage(img)
 
@@ -54,51 +50,50 @@ class TelaInicial(ctk.CTkFrame):
             size=(400, 600)  # tamanho desejado
         )
 
+        # Criação do Frame que comportará o conteúdo do frame direito
         self.background_label = ctk.CTkLabel(self.frame_direito, text='', image=self.background)
         self.background_label.grid(row=0, column=0, sticky='nsew')#.place(x=0, y=0, relwidth=1, relheight=1)#
 
-        # self.overlay_frame = ctk.CTkFrame(
-        #     self.background_label,
-        #     fg_color="tranpas"
-        # )
-        # self.overlay_frame.place(x=0, y=0, relwidth=1, relheight=1)
-        #
+        self.conteudo_frame = ctk.CTkFrame(self.background_label, fg_color='transparent')
+        self.conteudo_frame.grid(row=0, column=0)
 
-        frame = ctk.CTkFrame(self.background_label, fg_color='transparent')
-        frame.grid(row=0, column=0)
+        # self.conteudo_frame.grid_columnconfigure(0, weight=1)
+        # self.conteudo_frame.grid_columnconfigure(1, weight=1)
 
-        button = ctk.CTkButton(frame, text="Botão")
-        button.grid(row=0, column=0)#.place(relx=0.5, rely=0.6, anchor="center")
+        # Conteúdo do frame direito
+        self.texto_frame = ctk.CTkFrame(self.conteudo_frame, fg_color='transparent')
+        self.texto_frame.grid(row=0, column=0)
 
-        button2 = ctk.CTkButton(frame, text="Botão 2")
-        button2.grid(row=1, column=0)
+        self.bem_vindo_label = ctk.CTkLabel(self.texto_frame, text="Seja bem-vindo ao", text_color='black', font=('Poppins', 22), anchor='w')
+        self.bem_vindo_label.grid(row=0, column=0, sticky='w')#.pack(pady=10, side='left')
 
-
-        # self.grid_columnconfigure(0, weight=1)
-        # self.grid_columnconfigure(1, weight=1)
-        # # self.janela.grid_rowconfigure(0, weight=1)
-        # # self.janela.grid_rowconfigure(1, weight=1)
-        #
-        # self.titulo_janela_label = ctk.CTkLabel(self, text="Seja bem-vindo ao Gerador de Resoluções do PPGCTA", font=('Segoe UI', 25))
-        # self.titulo_janela_label.grid(row=0, column=0, columnspan=2, pady=10)
-        #
-        # self.tela_principal_button = ctk.CTkButton(self, text="Criar Resoluções", height=60, font=('Segoe UI', 15), command=self.master.exibir_tela_principal)\
-        #     .grid(row=1, column=0, sticky='ew', pady=20, padx=(10, 10))
-        # self.tela_gerencia_button = ctk.CTkButton(self, text="Gerenciar", height=60, font=('Segoe UI', 15), command=self.master.exibir_tela_gerencia)\
-        #     .grid(row=1, column=1, sticky='ew', pady=20, padx=(10, 10))
+        self.gerador_resolucoes_label = ctk.CTkLabel(self.texto_frame, text="Gerador de Resoluções do PPGCTA", text_color='#587800', font=('Poppins', 22, 'bold'), anchor='w')
+        self.gerador_resolucoes_label.grid(row=1, column=0, sticky='w')#.pack(pady=5, side='left')
 
 
-    # def inserir_imagem(self, img_path):
-    #
-    #     self.img_original = Image.open(img_path)
-    #
-    #     self.bg_label = ctk.CTkLabel(self.frame_esquerdo, text="")
-    #     self.bg_label.grid(row=0, column=0, sticky='nsew')
-    #
-    #     self.frame_esquerdo.grid_rowconfigure(0, weight=1)
-    #     self.frame_esquerdo.grid_columnconfigure(0, weight=1)
-    #
-    #     self.frame_esquerdo.bind("<Configure>", self.redimensionar_imagem)
+        self.botoes_frame = ctk.CTkFrame(self.conteudo_frame, fg_color='transparent')
+        self.botoes_frame.grid(row=1, column=0, pady=(50,0))
+
+        self.criar_button = ctk.CTkButton(self.botoes_frame, text="Criar Resoluções", fg_color='#749619', border_color='black', border_width=1, width=200, height=80, font=('Manrope', 22), corner_radius=20)
+        self.criar_button.grid(row=0, column=0, pady=10)
+
+        self.gerenciar_button = ctk.CTkButton(self.botoes_frame, text="Gerenciar", fg_color='#749619', border_color='black', border_width=1, width=200, height=80, font=('Manrope', 22), corner_radius=20)
+        self.gerenciar_button.grid(row=1, column=0)
+
+        self.logo_frame = ctk.CTkFrame(self.conteudo_frame, fg_color='transparent')
+        self.logo_frame.grid(row=2, column=0, pady=(20, 0))
+
+        img_logo = Image.open('src/static/img/PPGCTA.png')
+        # self.img_principal = ImageTk.PhotoImage(img)
+
+        self.logo = ctk.CTkImage(
+            light_image=img_logo,
+            dark_image=img_logo,  # opcional, mas recomendado
+            size=(110, 65)  # tamanho desejado
+        )
+
+        self.logo_label = ctk.CTkLabel(self.logo_frame, text='', image=self.logo, anchor='e')
+        self.logo_label.grid(row=0, column=0, sticky='e')
 
     # def redimensionar_imagem(self, event): # fará o ajuste automático do tamanho da imagem para que ocupe o frame esquerdo
     #     w, h = event.width, event.height
